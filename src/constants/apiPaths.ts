c800e82cebdf866cac756c5f609ca34d98b870ec
Maksim <maksim20051708@gmail.com>
2025-04-21 23:37:38 +0300

    feat: add correct route path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@ const API_PATHS = {
   product: "https://vecx6oo32g.execute-api.us-east-1.amazonaws.com/prod",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://op6xe9j1k8.execute-api.us-east-1.amazonaws.com/prod",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "http://maksimgurbanow-bff-service.us-east-1.elasticbeanstalk.com/",
   cart: "http://maksimgurbanow-cart-api.us-east-1.elasticbeanstalk.com/api",
 };
 
